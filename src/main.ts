39761eb82b03e96c1bb4295108f7552cfa3eb367
Eric Busboom <eric@busboom.org>
2025-08-02 14:07:52 -0700

    Update joystick module name and description in pxt.json; remove unnecessary code from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
 
 joystick.run();
 
-
-while (true) {
-    leagueir.sendIrAddressCommand_cpp(DigitalPin.P8, 0xFF00, 0xABCD);
-    pause(500);
-    leagueir.sendIrAddressCommand_cpp(DigitalPin.P8, 0x00FF, 0xABCD);
-    pause(500);
-}
