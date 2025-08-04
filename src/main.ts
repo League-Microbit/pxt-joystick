c771d0ccb43b1c40c0f0ab3916fdc59f574393c1
Eric Busboom <eric@busboom.org>
2025-08-04 08:22:44 -0700

    Add documentation comments for joystick transmitter functionality

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 
+
+/**
+ * Joystick transmitter
+ * This program reads the joystick and buttons and
+ * sends the values out to the radio. It will select
+ * an empty radio channel, which you can transmit
+ * to your Cutebot by pressing the A+B buttons.
+ */
 joystick.initFreeChannel(DigitalPin.P8)
 basic.forever(function () {
 	
 })
+
