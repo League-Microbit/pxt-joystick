c32a5b79be612b442c300599ce8b63106b9bb53a
Eric Busboom <eric.busboom@jointheleague.org>
2026-02-22 14:52:15 -0800

    Release version 1.20260222.1

--- src/main.ts
+++ src/main.ts
@@ -9,47 +9,46 @@
  */
 
 
-//joystick.initRadioOnFreeChannel(DigitalPin.P8)
+if (false) {
+    //joystick.initRadioOnFreeChannel(DigitalPin.P8)
 
-joystick.initRadioOnDefaultChannel(DigitalPin.P8)
-serial.writeLine('== Joystick Transmitter ==')
-serial.writeLine('Channel ' + radiop.getChannel() + ' group ' + radiop.getGroup());
-joystick.blockingRadioTransfer(radiop.DeviceClass.CUTEBOT);
+    joystick.initRadio(1, 1, DigitalPin.P8)
 
+    //radiop.initDefaults()
+    joystick.initJoystick();
+    joystick.calibrate();
 
-//radiop.initDefaults()
-joystick.initJoystick();
-joystick.calibrate();
+    basic.showIcon(IconNames.Heart);
 
-basic.showIcon(IconNames.Heart);
+    // Install handler for incoming Bot Status (print x,y,buttons) using specialized API
+    radiop.onReceiveJoystickMessage(function (bs: radiop.JoyPayload) {
+        // Decode 25-bit image value back to 5x5 and display
+        radiop.intToImage(bs.image).showImage(0);
+        bs.playSound();
 
-// Install handler for incoming Bot Status (print x,y,buttons) using specialized API
-radiop.onReceiveJoystickMessage(function (bs: radiop.JoyPayload) {
-    // Decode 25-bit image value back to 5x5 and display
-    radiop.intToImage(bs.image).showImage(0);
-    bs.playSound();
-
-});
+    });
         
-basic.forever(function () {
-    let jp = joystick.newJoyPayload();
+    basic.forever(function () {
+        let jp = joystick.newJoyPayload();
 
-    if (jp.buttonPressed(radiop.JoystickButton.A)) {
-        jp.setIcon(IconNames.Heart);
-    }   else if (jp.buttonPressed(radiop.JoystickButton.B)) {
-        jp.setIcon(IconNames.Sad);
-    } else if (jp.buttonPressed(radiop.JoystickButton.C)) {
-        jp.setIcon(IconNames.Happy);
-    }   else if (jp.buttonPressed(radiop.JoystickButton.D)) {
-        jp.setIcon(IconNames.Giraffe);
-    }   else if (jp.buttonPressed(radiop.JoystickButton.E)) {
-        jp.setIcon(IconNames.Ghost);
-    }   else if (jp.buttonPressed(radiop.JoystickButton.F)) {
-        jp.setIcon(IconNames.Skull);
-    }
+        if (jp.buttonPressed(radiop.JoystickButton.A)) {
+            jp.setIcon(IconNames.Heart);
+        } else if (jp.buttonPressed(radiop.JoystickButton.B)) {
+            jp.setIcon(IconNames.Sad);
+        } else if (jp.buttonPressed(radiop.JoystickButton.C)) {
+            jp.setIcon(IconNames.Happy);
+        } else if (jp.buttonPressed(radiop.JoystickButton.D)) {
+            jp.setIcon(IconNames.Giraffe);
+        } else if (jp.buttonPressed(radiop.JoystickButton.E)) {
+            jp.setIcon(IconNames.Ghost);
+        } else if (jp.buttonPressed(radiop.JoystickButton.F)) {
+            jp.setIcon(IconNames.Skull);
+        }
 
-    if(radiop.sendIfChanged(jp)){
-        basic.pause(100)
-    }
+        if (radiop.sendIfChanged(jp)) {
+            basic.pause(100)
+        }
 
-})
+    })
+    
+}
